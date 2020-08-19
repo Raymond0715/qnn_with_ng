Fix bug about natural gradient.

main.py
@@ -86,11 +86,21 @@ class NGalpha(tf.keras.callbacks.Callback):
         
     def on_epoch_begin(self, epoch, logs = None):
         # pdb.set_trace()
+        # print('[DEBUG][main.py] learning rate:', self.model.optimizer.lr.numpy())
+        # print('[DEBUG][main.py] alpha, before epoch begin:', self.model.alpha)
         self.model.alpha = \
                 1.0 / (math.e - 1.0) * \
                 (math.e ** (float(epoch) / self.model.num_epochs) - 1)
-        print('[INFO][main.py] alpha:', self.model.alpha)
-        print('[INFO][main.py] learning rate:', self.model.optimizer.lr.numpy())
+        # print('[DEBUG][main.py] alpha, after epoch begin:', self.model.alpha)
+
+    def on_test_begin(self, logs = None):
+        # print('[DEBUG][main.py] alpha, before test batch:', self.model.alpha)
+        self.model.alpha = 1
+        # print('[DEBUG][main.py] alpha, after test batch:', self.model.alpha)
+
+    # def on_test_batch_begin(self, batch, logs = None):
+        # print('[DEBUG][main.py] alpha, test batch:', self.model.alpha)
+
 
 
 if __name__ == '__main__':

nn_utils.py
@@ -60,7 +60,9 @@ class QConv2D(tf.keras.layers.Layer):
         elif self.quantilize == 'ng':
             quantize_filters = self.QuantilizeWeight(self.filters)
             filters = tangent(self.filters, quantize_filters, self.alpha)
-            input_tensor = self.QuantilizeActivation(input_tensor)
+            input_tensor_quantilize = self.QuantilizeActivation(input_tensor)
+            input_tensor = tangent(
+                    input_tensor, input_tensor_quantilize, self.alpha)
         else:
             filters = self.filters
 

quantization.py
@@ -18,9 +18,10 @@ def QuantilizeFnSTE(Wbit, Abit):
     @tf.custom_gradient
     def QuantilizeWeight(w):
         if Wbit == 1:   # BNN
-            mean = tf.reduce_mean(tf.abs(w))
-            E = tf.stop_gradient(mean)
-            output = tf.sign(w / E) * E
+            # mean = tf.reduce_mean(tf.abs(w))
+            # E = tf.stop_gradient(mean)
+            # output = tf.sign(w / E) * E
+            output = tf.sign(w)
         elif Wbit == 32:
             output = w
         else:   # QNN
@@ -37,9 +38,10 @@ def QuantilizeFnSTE(Wbit, Abit):
     @tf.custom_gradient
     def QuantilizeActivation(x):
         if Abit == 1:   # BNN
-            mean = tf.reduce_mean(tf.abs(x))
-            E = tf.stop_gradient(mean)
-            output = tf.sign(x / E) * E
+            # mean = tf.reduce_mean(tf.abs(x))
+            # E = tf.stop_gradient(mean)
+            # output = tf.sign(x / E) * E
+            output = tf.sign(x)
         elif Abit == 32:
             output = x
         else:   # QNN